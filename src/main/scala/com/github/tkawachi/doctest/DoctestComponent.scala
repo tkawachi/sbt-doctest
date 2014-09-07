f908916500c3f4f59a3f371e087777497906726b
Frank S. Thomas <frank@timepit.eu>
2014-09-07 22:10:29 +0200

    Add test with parametric type

--- src/main/scala/com/github/tkawachi/doctest/DoctestComponent.scala
+++ src/main/scala/com/github/tkawachi/doctest/DoctestComponent.scala
@@ -1,7 +1,5 @@
 package com.github.tkawachi.doctest
 
-case class TestResult(value: String, tpe: Option[String] = None)
-
 sealed abstract class DoctestComponent
 case class Example(expr: String, expected: TestResult, line: Int) extends DoctestComponent
 case class Prop(prop: String, line: Int) extends DoctestComponent
---
+++ src/main/scala/com/github/tkawachi/doctest/TestResult.scala
@@ -0,0 +1,3 @@
+package com.github.tkawachi.doctest
+
+case class TestResult(value: String, tpe: Option[String] = None)
