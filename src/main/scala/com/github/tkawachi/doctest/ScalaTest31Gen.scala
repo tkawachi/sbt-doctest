aa330365aa1852e96aa4d5706543c453200af357
Takashi Kawachi <tkawachi@gmail.com>
2019-06-09 17:02:13 +0900

    Used scalatestplus-scalacheck

--- src/main/scala/com/github/tkawachi/doctest/ScalaTest31Gen.scala
+++ src/main/scala/com/github/tkawachi/doctest/ScalaTest31Gen.scala
@@ -4,7 +4,7 @@ package com.github.tkawachi.doctest
  * Test generator for ScalaTest >= 3.1.0.
  */
 object ScalaTest31Gen extends ScalaTestGen {
-  override protected def withCheckersString: String = "with _root_.org.scalatest.check.Checkers"
+  override protected def withCheckersString: String = "with _root_.org.scalatestplus.scalacheck.Checkers"
 
   override protected def funSpecClass: String = "_root_.org.scalatest.funspec.AnyFunSpec"
 }
